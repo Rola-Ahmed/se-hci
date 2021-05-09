3322f5f5da05e95945ac8c7e7579a74c9ebee402
Rola-Ahmed <rolaahmed1807542@miuegypt.edu.eg>
2021-05-09 16:34:47 +0200

    se folder uploaded
    .

---
+++ se/home.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="zina.css" rel="stylesheet">
+<link href="ImageSlideShow.css" rel="stylesheet">
+    
+    <!--bootstrap icon link---  /1--bootstrap
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
+  <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"/>
+  <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"/>
+</svg-->
+    <!--icon-->
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+    
+    <div class='logo'>  
+   <img src='image/logo.jpg' alt='Official logo' width='150px' height='100px' text="Dental House cure clinic"> 
+        
+</div> 
+
+<div class="navbar">
+    
+    <a href="signup.php">Sign Up</a>
+    <a href="Login.php">Log In</a>
+     <a href="Clinic.php">Clinic</a>
+     <a href="Contact Us.php">Contact Us</a>
+    <a href="About.php">About</a>
+    <a href="About.php">About</a>
+    <a href="Home.php">Home</a>
+    
+</div>
+    
+    
+   
+    
+    <div class ="images" ><img src="image/homeImage.jpg" width="100%" height="100%" /></div>
+   <!--img style="float: right; margin: 0px 100px 20px 0px;" src="image/your-image.png" width="100" /-->
+
+    <div class="about">
+    
+    <h1 style="text-align:left;">About</h1>
+    <h4>we provide dentists cutting-edges solutions to digitize their clinic.
+    from practicie management software tobuilding websites,
+    branding, marketing and more. Our aim is to provide dentisits with all necessary tools to rake their practice to new heights.
+        </h4>    
+    
+    </div>
+    
+    <div class="instaFace">  
+        <a href="">  
+          <i class="fab fa-instagram-square"></i>
+        </a>
+        
+        <a href="">  
+        <i class="fab fa-facebook-f"></i>
+        </a>
+    
+    </div>
+
+</body>
+</html>
